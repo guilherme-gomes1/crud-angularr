2a950edde3f51e8d8565956b3efb284cd5ab4e6a
Guilherme Gomes <guilhermegg018@gmail.com>
2023-07-23 00:53:02 -0300

    criando o model e a tabela que sera exibida os cursos

--- src/app/courses/courses.module.ts
+++ src/app/courses/courses.module.ts
@@ -4,14 +4,18 @@ import { CommonModule } from '@angular/common';
 import { CoursesRoutingModule } from './courses-routing.module';
 import { CoursesComponent } from './courses/courses.component';
 
+import { MatTableModule } from '@angular/material/table';
+import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
-  declarations: [
-    CoursesComponent
-  ],
+  declarations: [CoursesComponent],
   imports: [
     CommonModule,
-    CoursesRoutingModule
-  ]
+    CoursesRoutingModule,
+    MatTableModule,
+    MatCardModule,
+    MatToolbarModule,
+  ],
 })
-export class CoursesModule { }
+export class CoursesModule {}
