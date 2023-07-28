0612300d4c6c987ed29710deda991c6c037da8d0
Guilherme Gomes <guilhermegg018@gmail.com>
2023-07-28 08:54:10 -0300

    adicionando proxy para conseguir acessar o back-end

--- src/app/courses/services/courses.service.ts
+++ src/app/courses/services/courses.service.ts
@@ -2,13 +2,13 @@ import { Injectable } from '@angular/core';
 import { Course } from '../model/course';
 
 import { HttpClient } from '@angular/common/http';
-import { Observable, delay, first, tap } from 'rxjs';
+import { delay, first, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CoursesService {
-  private readonly API = '/assets/courses.json';
+  private readonly API = 'api/courses';
 
   constructor(private httpClient: HttpClient) {}
 
